5970e50dfdd038e3828360005f0472b86d841637
Matthias Ehrendorfer <matthias.ehrendorfer@gmail.com>
2025-06-26 20:51:36 +0200

    clean up cpee-llm.rb

--- server/cpee-llm.rb
+++ server/cpee-llm.rb
@@ -3,7 +3,6 @@ require 'riddl/server'
 require 'riddl/client'
 require 'xml/smart'
 require 'json'
-require 'typhoeus'
 require_relative '../lib/cpee/llm/llm_functions'
 
 def cpee_to_mermaid(cpee) #{{{
@@ -21,7 +20,6 @@ def cpee_to_mermaid(cpee) #{{{
 end #}}}
 
 def mermaid_to_cpee(mermaid) #{{{
-  #srv = Riddl::Client.new('http://localhost:9295/mermaid')
   srv = Riddl::Client.new('http://localhost:9295/cpee/mermaid')
   status, res = srv.post [
     Riddl::Parameter::Complex.new("description","text/plain",mermaid),
@@ -33,9 +31,6 @@ def mermaid_to_cpee(mermaid) #{{{
     puts mermaid
     raise 'error when converting mermaid to cpee'
   end
-  pp res
-  pp res[0].value().read()
-  res[0].value().rewind()
   return res[0].value().read()
 end #}}}
 
@@ -47,10 +42,6 @@ class CreateMermaid < Riddl::Implementation #{{{
       user_input = @p[1].value().read()
       myllm = @p[2].value().read()
       doc = XML::Smart.string(input_cpee)
-      pp "incoming cpee: #{doc.to_s()}"
-      File.write('incoming_cpee.xml',doc.to_s())
-      pp "incoming user_input: #{user_input}"
-      pp "used model: #{myllm}"
     rescue Exception => e
       @status = 400
       return Riddl::Parameter::Complex.new("llm_out","application/json",{:error => e}.to_json())
@@ -60,7 +51,6 @@ class CreateMermaid < Riddl::Implementation #{{{
       begin
         llm_response = generate_model(myllm,user_input)
       rescue LLMError => e
-        pp "LLMError #{e.http_response}"
         @status = e.http_response
         return Riddl::Parameter::Complex.new("llm_out","application/json",{:error => e.message}.to_json())
       end
@@ -68,40 +58,19 @@ class CreateMermaid < Riddl::Implementation #{{{
       begin
         llm_response = adapt_model(myllm,doc,user_input)
       rescue LLMError => e
-        pp "LLMError #{e.http_response}"
         @status = e.http_response
         return Riddl::Parameter::Complex.new("llm_out","application/json",{:error => e.message}.to_json())
       end
     end
 
-=begin
-    # make a call to llm
-    #myllm = "gemini-2.0-flash"
     begin
-      final_response = main_function(myllm, doc, user_input)
-    rescue LLMError => e
-      puts e.message
-      final_response = llm_error_handling(e.llm_response, myllm, method(:main_function), doc, user_input)
-    end
-=end
-
-    # convert llm response to cpee tree
-    # only for testing
-    # final_response = File.read(File.join(__dir__,'mermaid_example'))
-    # only for testing
-    begin
-      pp "outgoing mermaid: #{llm_response}"
-      File.write('outgoing_mermaid.mmd',llm_response)
       output_cpee = mermaid_to_cpee(llm_response)
-      pp "outgoing cpee: #{output_cpee}"
-      File.write('outgoing_cpee.xml',output_cpee)
     rescue Exception => e
       @status = 500
       return Riddl::Parameter::Complex.new("llm_out","application/json",{:error => e}.to_json())
     end
 
     return(Riddl::Parameter::Complex.new("llm_out","application/json",{:user_input => user_input, :used_llm => myllm, :input_cpee => input_cpee, :input_intermediate => doc.root().empty?() ? "" : cpee_to_mermaid(doc.to_s()), :output_intermediate => llm_response, :output_cpee => output_cpee, :status => "success"}.to_json()))
-    #return Riddl::Parameter::Complex.new("llm_out","application/json",{:input_cpee => input_cpee, :input_intermediate => "", :output_intermediate => llm_response, :output_cpee => output_cpee, :status => ""}.to_json())
   end
 end #}}}
 
@@ -112,9 +81,6 @@ class CreateText < Riddl::Implementation #{{{
       input_cpee = @p[0].value.read
       myllm = @p[1].value
       doc = XML::Smart.string(input_cpee)
-      pp "incoming cpee: #{doc.to_s}"
-      File.write('incoming_cpee.xml',doc.to_s)
-      pp "used model: #{myllm}"
     rescue Exception => e
       @status = 400
       return Riddl::Parameter::Complex.new('llm_out','application/json',{:error => e}.to_json())
@@ -127,14 +93,11 @@ class CreateText < Riddl::Implementation #{{{
       begin
         llm_response = generate_text(myllm,doc)
       rescue LLMError => e
-        pp "LLMError #{e.http_response}"
         @status = e.http_response
         return Riddl::Parameter::Complex.new('text_out','application/json',{:error => e.message}.to_json())
       end
     end
     begin
-      pp "outgoing text: #{llm_response}"
-      File.write('outgoing_text.txt',llm_response)
       output_cpee = mermaid_to_cpee(llm_response)
     rescue Exception => e
       @status = 500
